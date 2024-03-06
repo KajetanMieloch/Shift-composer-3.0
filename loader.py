ab3f6e0cd4eac4333a87e29ea5389ef3839a62c5
KajetanMieloch <kajetanmieloch@gmail.com>
2024-03-06 11:43:04 +0100

    Add Loader class
    Loader is class that obtain all data from jsons

--- loader.py
+++ loader.py
@@ -1,17 +1,27 @@
 import os
 import json
 
-folder_path = 'userData'
-file_names = ['allEmp.json', 'empProp.json', 'scheduleProp.json']
+class Loader:
+    def __init__(self):
+        self.folder_path = 'userData'
+        self.file_names = ['allEmp.json', 'empProp.json', 'scheduleProp.json']
+        self.data = {}
 
-data = {}
+        for file_name in self.file_names:
+            file_path = os.path.join(self.folder_path, file_name)
+            with open(file_path) as file:
+                self.data[file_name] = json.load(file)
 
-for file_name in file_names:
-    file_path = os.path.join(folder_path, file_name)
-    with open(file_path) as file:
-        data[file_name] = json.load(file)
+        # Access the loaded JSON data
+        self.all_emp_data = self.data['allEmp.json']
+        self.emp_prop_data = self.data['empProp.json']
+        self.schedule_prop_data = self.data['scheduleProp.json']
 
-# Access the loaded JSON data
-all_emp_data = data['allEmp.json']
-emp_prop_data = data['empProp.json']
-schedule_prop_data = data['scheduleProp.json']
+    def get_all_emp_data(self):
+        return self.all_emp_data
+
+    def get_emp_prop_data(self):
+        return self.emp_prop_data
+
+    def get_schedule_prop_data(self):
+        return self.schedule_prop_data
