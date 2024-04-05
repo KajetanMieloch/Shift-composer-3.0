f6e254e52c33fee453013b00adca9050c2a78b97
KajetanMieloch <kajetanmieloch@gmail.com>
2024-04-05 12:13:41 +0200

    Notes
    Now i have a few ideas i would describe for future me.
    Do not try to do any cross-department modification (main). Do it after Phase 1 is finished.
    
    Phase 1 is an extension for generator method to optimise schedule. Now after phase 0 employees fulled schedule.
    Now Phase 1 is going to delete some shifts, but still to target.

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import loader
 import init
 import generator as gen
 
-def generate(department):
+def generate(department, priority = 0):
+    #priority is used when one employee is available for multiple departments
+    #priority 0 means that none of the departments is prioritized, so employee will be randomly selected, try to avoid this
     data_loader = loader.Loader()
     employeesAndAvailability = init.EmployeesAndAvailability(data_loader.get_all_employeesloyees_data(), data_loader.get_employees_properties_data(), data_loader.get_schedule_properties_data())
     sheduleProperties = init.ScheduleProperties(data_loader.get_schedule_properties_data())
@@ -13,23 +15,32 @@ def generate(department):
     
     ## Debug ###
     for h in harmonogram:
-        print(h.start_date, h.end_date, h.start_hour, h.end_hour, h.matched_employees)
+        #print(h.start_date, h.end_date, h.start_hour, h.end_hour, h.matched_employees)
         for emp in h.matched_employees:
-            print(emp.get_employee_avability_for_department())
+            #print(emp.get_employee_avability_for_department())
             pass
     ## Debug ###
         
+    #This return is marked to delete
+    #Instead of returning the harmonogram, we should process it and return optimized schedule
     return {"department": department, "harmonogram": harmonogram}
 
+
 def get_schedule_to_combine(schedules):
+    """
+    Retrieves the schedules that need to be combined based on the shared employees' availability for multiple departments.
+
+    Args:
+        schedules (list): A list of schedules.
+
+    Returns:
+        list: A list of schedules to be combined, where each schedule is represented as a list of department names.
+    """
     shared_employees = []
     schedules_to_combine = []
     for s in schedules:
         for h in s["harmonogram"]:
             for emp in h.matched_employees:
-                #THIS IS THE EASY WAY TO DO IT
-                #I PROGRAMED ALL METHOS BEFORE NOW I IT SO EASY
-                #emp.get_employee_avability_for_department() is a dictionary with the hours of availability for each department
                 if len(emp.get_employee_avability_for_department().keys()) > 1:
                     if emp not in shared_employees and emp.get_id() not in [e.get_id() for e in shared_employees]:
                         shared_employees.append(emp)
@@ -56,8 +67,8 @@ def combine(schedules, schedules_to_combine):
 
 def main():
 
-    schedules_multiple_departments = []
     schedules_single_department = []
+    schedules_multiple_departments = []
 
     schedules = []
     schedules.append(generate("HR"))
@@ -72,6 +83,7 @@ def main():
             schedules_multiple_departments.append(s)
         else:
             schedules_single_department.append(s)
+
 
 if __name__ == "__main__":
     main()
