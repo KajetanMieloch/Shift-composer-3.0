76521cb2098030534af58c4319986c94cd843903
KajetanMieloch <kajetanmieloch@gmail.com>
2024-05-14 15:49:20 +0200

    Add new tab and add option to select multiple departments

--- GUIJsonGenerator.py
+++ GUIJsonGenerator.py
@@ -1,6 +1,6 @@
 import sys
 import json
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QListWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QListWidget, QPushButton, QTabWidget, QAbstractItemView
 
 class JSONGenerator(QWidget):
     def __init__(self):
@@ -10,31 +10,40 @@ class JSONGenerator(QWidget):
     def initUI(self):
         self.layout = QVBoxLayout()
 
-        self.employee_layout = QVBoxLayout()
-        self.employee_layout.addWidget(QLabel("Employee Details"))
+        # Tab widget to manage multiple tabs
+        self.tab_widget = QTabWidget()
 
+        # First tab for adding employees
+        self.employee_tab = QWidget()
+        self.employee_layout = QVBoxLayout(self.employee_tab)
+        self.employee_layout.addWidget(QLabel("Employee Details"))
         self.name_input = QLineEdit()
         self.employee_layout.addWidget(QLabel("Name:"))
         self.employee_layout.addWidget(self.name_input)
-
         self.surname_input = QLineEdit()
         self.employee_layout.addWidget(QLabel("Surname:"))
         self.employee_layout.addWidget(self.surname_input)
-
         self.department_input = QListWidget()
+        self.department_input.setSelectionMode(QAbstractItemView.MultiSelection)  # Set selection mode to allow multiple selections
         self.department_input.addItems(["HR", "IT", "Security"])
         self.employee_layout.addWidget(QLabel("Department(s):"))
         self.employee_layout.addWidget(self.department_input)
-
-        self.layout.addLayout(self.employee_layout)
-
         self.add_button = QPushButton("Add Employee")
         self.add_button.clicked.connect(self.addEmployee)
-        self.layout.addWidget(self.add_button)
-
+        self.employee_layout.addWidget(self.add_button)
         self.generate_button = QPushButton("Generate JSON")
         self.generate_button.clicked.connect(self.generateJSON)
-        self.layout.addWidget(self.generate_button)
+        self.employee_layout.addWidget(self.generate_button)
+        self.tab_widget.addTab(self.employee_tab, "Employees")
+
+        # Second tab for employee properties
+        self.employee_properties_tab = QWidget()
+        self.employee_properties_layout = QVBoxLayout(self.employee_properties_tab)
+        self.sample_text_button = QPushButton("Sample Text")
+        self.employee_properties_layout.addWidget(self.sample_text_button)
+        self.tab_widget.addTab(self.employee_properties_tab, "Employee Properties")
+
+        self.layout.addWidget(self.tab_widget)
 
         self.setLayout(self.layout)
         self.setWindowTitle("JSON Generator")
@@ -51,7 +60,7 @@ class JSONGenerator(QWidget):
                 "id": len(self.employees) + 1 if self.employees else 1,  # Incremented ID if employees exist
                 "name": name,
                 "surname": surname,
-                "department": [item.text() for item in self.department_input.selectedItems()]
+                "departments": [item.text() for item in self.department_input.selectedItems()]
             }
             self.employees.append(employee)
             self.name_input.clear()
