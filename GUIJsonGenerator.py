5dfcfcb7de4d23d2ca0ca9b4feb3d93745847227
KajetanMieloch <kajetanmieloch@gmail.com>
2024-05-14 15:20:39 +0200

    Add saving output to JSON

--- GUIJsonGenerator.py
+++ GUIJsonGenerator.py
@@ -59,7 +59,9 @@ class JSONGenerator(QWidget):
     def generateJSON(self):
         data = {"employees": self.employees}
         json_data = json.dumps(data, indent=4)
-        print(json_data)  # You can replace this with saving to a file or displaying in a text box
+        with open("allemployees.json", "w") as json_file:
+            json_file.write(json_data)
+        print("JSON data saved to allemployees.json")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
